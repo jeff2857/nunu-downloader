65052e164055c7a7a9417ff3ec78d73fcb2b7f11
jeff2857 <jeffliu2857@gmail.com>
2022-08-05 18:42:28 +0800

    feat: trim body

--- src/parser/page_parser.rs
+++ src/parser/page_parser.rs
@@ -6,8 +6,11 @@ impl PageParser {
     pub fn parse_index(body: String) -> Novel {
         let mut novel = Novel::default();
 
+        let body = Self::trim_body(&body);
+
         let title = Self::find_novel_title(&body);
         let author = Self::find_novel_author(&body);
+        let chapters = Self::find_novel_chapters(&body);
 
         novel.title = title;
         novel.author = author;
@@ -17,6 +20,22 @@ impl PageParser {
 
     pub fn parse_chapter_page(body: String) -> Chapter {
         Chapter::default()
+    }
+
+    fn trim_body(body: &str) -> String {
+        let mut begin = 0;
+        for _ in 0..8 {
+            let e = body[begin..].find(r"</table>").unwrap();
+            begin += e + 1;
+        }
+
+        let mut end = begin + body[begin..].rfind(r"努努书坊").unwrap();
+        for _ in 0..2 {
+            let e = body[begin..end].rfind("</table>").unwrap();
+            end = begin + e;
+        }
+
+        body[begin..end].to_string()
     }
 
     fn find_novel_title(body: &str) -> String {
@@ -37,6 +56,12 @@ impl PageParser {
     }
 
     fn find_novel_chapters(body: &str) -> Vec<Chapter> {
+        let chapter_pattern = r#"><a href="(?P<href>.*)">(?P<chp>.*)</a></td>"#;
+        let re = Regex::new(chapter_pattern).unwrap();
+        for cap in re.captures_iter(body) {
+            let chp = &cap["chp"];
+            println!("chp: {}", chp);
+        }
         vec![]
     }
 }
